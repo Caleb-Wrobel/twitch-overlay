cb40aa45c74c9dc17e4c9ae5306ddbb4e8e25663
Samantha Medeiros <samanthamed1@gmail.com>
2016-11-26 02:41:22 -0500

    Initial implementation of ticker text script side

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+	// test quotes from Fantastic Beasts and Where to Find Them
+	var testQuotes = [
+	"\"Imperfect understanding is often more dangerous than ignorance.\"",
+	"\"My philosophy is that worrying means you suffer twice.\"",
+	"\"I ain't got the brains to make this up.\"",
+	"\"See, they're currently in alien terrain, surrounded by millions of the most vicious creatures on the planet. Humans.\"",
+	"\"Hey, Mr English guy! I think your egg is hatching.\""
+	]
+
+	// randomizer for the quote array so it's a different order each time
+	function randomize(arr) {
+		var temp = arr.slice();
+		var randomized = [];
+		for (var i = arr.length; i > 0; i--) {
+			var randomPosition = Math.floor(Math.random() * i);
+			randomized.push(temp[randomPosition]);
+			temp.splice(randomPosition, 1);
+		}
+		return randomized;
+	}
+
+	// display randomized quotes in ticker area
+	$('.ticker').append('<marquee scrollamount="8" class="ticker-text">' + randomize(testQuotes).join(" ... ") + '</marquee>');
+
+});
